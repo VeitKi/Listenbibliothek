b6899f6313b70d40b2953be7ac075c453af5f548
Veit Kiebler <veit.kiebler@cgm.com>
2019-05-02 19:26:30 +0200

    adding gitignore file

---
+++ src/de/dhbw/ravensburg/List.java
@@ -0,0 +1,18 @@
+package de.dhbw.ravensburg;
+
+public interface List <T> {
+
+
+        public boolean isEmpty();
+        public int size();
+        public void addElement(T value);
+        public void addOtherList(List list);
+        public void removeAll();
+        public boolean contains(T value);
+        public boolean containsAll(List list);
+        public void removeCertain(T value);
+        public T[] returnAsArray();
+
+        public String toString();
+
+}

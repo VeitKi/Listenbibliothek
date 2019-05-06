0ff153e05948c10cb6c3c3bc747ca3637b5670a2
Veit Kiebler <veit.kiebler@cgm.com>
2019-05-06 15:35:09 +0200

    Merge branch 'master' of https://github.com/VeitKi/istenbibliothek into dev
    # Conflicts:
    #	src/de/dhbw/ravensburg/Stack/Stack.java

--- src/de/dhbw/ravensburg/Interface/List.java
+++ src/de/dhbw/ravensburg/Interface/List.java
@@ -2,17 +2,71 @@ package de.dhbw.ravensburg.Interface;
 
 public interface List <T>{
 
+        /**
+         * Checks if the list is empty
+         * @return returns true if the list is empty
+         */
+        boolean isEmpty();
 
-        public boolean isEmpty();
-        public int size();
-        public void addElement(T value);
-        public void addOtherList(List list);
-        public void removeAll();
-        public boolean contains(T value);
-        public boolean containsAll(List list);
-        public void removeCertain(T value);
-        public T[] returnAsArray();
 
-        public String toString();
+        /**
+         * Checks the number of elements in the list
+         * @return returns the number of elements in the list
+         */
+        int size();
+
+
+        /**
+         * Adds an element to the list
+         * @param value the value added to the list
+         */
+        void addElement(T value);
+
+
+        /**
+         * Adds all objects from an other list to the current one
+         * @param list objects are added from this list to the current one
+         */
+        void addOtherList(List list);
+
+
+        /**
+         * Removes all objects from the list
+         */
+        void removeAll();
+
+
+        /**
+         * Checks if the list contains a certain element
+         * @param value value to check for
+         * @return returns true if the list contains the element
+         */
+        boolean contains(T value);
+
+
+        /**
+         * Checks if the committed list contains the same elements as the current list
+         * @param list do the elements of this list accord to the current one?
+         * @return returns true if the committed list contains the same elements as the current list
+         */
+        boolean containsAll(List list);
+
+
+        /**
+         * Removes certain element
+         * @param value element to remove
+         */
+        void removeCertain(T value);
+
+
+        /**
+         * Returns list as Array
+         * @return the list as array
+         */
+        T[] returnAsArray();
+
+
+
+        String toString();
 
 }

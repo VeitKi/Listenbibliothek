66fe7f0af31d26c61a47038286a225ce251dc283
Veit Kiebler <veit.kiebler@cgm.com>
2019-05-06 14:11:45 +0200

    adding gitignore file

--- src/de/dhbw/ravensburg/Queue/Queue.java
+++ src/de/dhbw/ravensburg/Queue/Queue.java
@@ -1,4 +1,51 @@
 package de.dhbw.ravensburg.Queue;
 
-public class Queue {
+import de.dhbw.ravensburg.Interface.List;
+
+public class Queue implements List {
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void addElement(Object value) {
+
+    }
+
+    @Override
+    public void addOtherList(List list) {
+
+    }
+
+    @Override
+    public void removeAll() {
+
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(List list) {
+        return false;
+    }
+
+    @Override
+    public void removeCertain(Object value) {
+
+    }
+
+    @Override
+    public Object[] returnAsArray() {
+        return new Object[0];
+    }
 }

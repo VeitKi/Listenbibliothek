6ffc30a05df45c899a7fe35f5f04fea4baa95bff
Veit Kiebler <veit.kiebler@cgm.com>
2019-05-06 14:25:09 +0200

    adding gitignore file

--- src/de/dhbw/ravensburg/Stack/Stack.java
+++ src/de/dhbw/ravensburg/Stack/Stack.java
@@ -2,50 +2,6 @@ package de.dhbw.ravensburg.Stack;
 
 import de.dhbw.ravensburg.Interface.List;
 
-public class Stack implements List {
+public class Stack {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void addElement(Object value) {
-
-    }
-
-    @Override
-    public void addOtherList(List list) {
-
-    }
-
-    @Override
-    public void removeAll() {
-
-    }
-
-    @Override
-    public boolean contains(Object value) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(List list) {
-        return false;
-    }
-
-    @Override
-    public void removeCertain(Object value) {
-
-    }
-
-    @Override
-    public Object[] returnAsArray() {
-        return new Object[0];
-    }
 }

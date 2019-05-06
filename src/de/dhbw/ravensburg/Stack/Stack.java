dc65f6b7e0bf5a970fa1404e82b9a1060ff7f9a2
caro999999 <50207918+caro999999@users.noreply.github.com>
2019-05-06 12:30:42 +0200

    Update Stack.java

--- src/de/dhbw/ravensburg/Stack/Stack.java
+++ src/de/dhbw/ravensburg/Stack/Stack.java
@@ -1,4 +1,82 @@
 package de.dhbw.ravensburg.Stack;
 
-public class Stack {
+public class Stack<T> {
+  
+  
+  //Variablendeklaration
+
+    public Stack<T> next;
+
+    private T variable;
+
+
+    //Konstruktor
+
+    public Stack(T variable) {
+
+        this.variable= variable;
+
+    }
+
+    //Ein neues Element wird dem Stack hinzugefügt
+
+    public void addElement(T item) {
+
+        next= this;
+
+        variable= item;
+
+    }
+
+    //Prüfung, ob der Stack leer ist oder Elemente enthält
+
+    public boolean isEmpty() {
+
+        if (variable == null) {
+
+            return true;
+        }
+
+        else {
+
+
+            return false;
+        }
+
+    }
+
+    //Gebe das oberste Element des Stacks zurück
+
+    public T peek (){
+
+
+        return variable;
+    }
+
+    //Den kompletten Stackinhalt löschen
+
+    public void clear() {
+
+        next = null;
+        variable = null;
+    }
+
+    int n;
+
+    public int size() {
+
+        T curElement = variable;
+
+
+        for(int i=0; curElement!=null; i++) {
+
+            curElement=curElement.next();
+
+        }
+
+        return n;
+
+    } 
+  
+}
 }

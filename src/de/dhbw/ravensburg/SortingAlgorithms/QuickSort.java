94165a2c17fdcb094441ceeca337efe404f5f3a9
Veit Kiebler <veit.kiebler@cgm.com>
2019-05-08 14:26:37 +0200

    Merge branch 'master' of https://github.com/VeitKi/istenbibliothek into dev
    # Conflicts:
    #	src/de/dhbw/ravensburg/Stack/Stack.java

--- src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
+++ src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
@@ -5,9 +5,9 @@ public class QuickSort {
     private int[] numbers;
     private int number;
 
-    public void sort(int[] values) {
-        // check for empty or null array
-        if (values ==null || values.length==0){
+    public void quickSort(int[] values) {
+        // check if array is empty or null
+        if (values == null || values.length == 0){
             return;
         }
         this.numbers = values;
@@ -17,29 +17,30 @@ public class QuickSort {
 
     private void quicksort(int low, int high) {
         int i = low, j = high;
-        // Get the pivot element from the middle of the list
-        int pivot = numbers[low + (high-low)/2];
+        // Get middle element of the list
+        int middle = numbers[low + (high-low)/2];
 
         // Divide into two lists
         while (i <= j) {
-            // If the current value from the left list is smaller than the pivot
+
+            // If the current value from the left list is smaller than the middle
             // element then get the next element from the left list
-            while (numbers[i] < pivot) {
+            while (numbers[i] < middle) {
                 i++;
             }
-            // If the current value from the right list is larger than the pivot
+            // If the current value from the right list is larger than the middle
             // element then get the next element from the right list
-            while (numbers[j] > pivot) {
+            while (numbers[j] > middle) {
                 j--;
             }
 
             // If we have found a value in the left list which is larger than
-            // the pivot element and if we have found a value in the right list
-            // which is smaller than the pivot element then we exchange the
+            // the middle element and if we have found a value in the right list
+            // which is smaller than the middle element then we exchange the
             // values.
             // As we are done we can increase i and j
             if (i <= j) {
-                exchange(i, j);
+                changeNumbers(i, j);
                 i++;
                 j--;
             }
@@ -51,7 +52,7 @@ public class QuickSort {
             quicksort(i, high);
     }
 
-    private void exchange(int i, int j) {
+    private void changeNumbers(int i, int j) {
         int temp = numbers[i];
         numbers[i] = numbers[j];
         numbers[j] = temp;

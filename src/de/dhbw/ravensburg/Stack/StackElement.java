225d2e9e01e7bc9b512df268a540fc3748f0ea65
caro999999 <50207918+caro999999@users.noreply.github.com>
2019-05-27 15:18:18 +0200

    Update StackElement.java

--- src/de/dhbw/ravensburg/Stack/StackElement.java
+++ src/de/dhbw/ravensburg/Stack/StackElement.java
@@ -1,4 +1,27 @@
 package de.dhbw.ravensburg.Stack;
 
 public class StackElement<T> {
+
+    public Comparable value;
+    public StackElement next;
+
+    public Comparable getValue(){
+
+        return this.value;
+    }
+
+    public void setValue(Comparable value){
+
+        this.value = value;
+    }
+
+    public StackElement<T> getNext(){
+
+        return this.next;
+    }
+
+    public void setNext(StackElement next){
+
+        this.next = next;
+    }
 }

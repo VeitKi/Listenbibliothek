3933ba54066fc36643577923290d8b8945e4c306
caro999999 <50207918+caro999999@users.noreply.github.com>
2019-06-03 20:32:15 +0200

    Update QuickSort.java

--- src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
+++ src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
@@ -1,61 +1,44 @@
 package de.dhbw.ravensburg.SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
-    private int[] numbers;
-    private int number;
+        public static void quickSort(int[] arr, int start, int end){
 
-    public void quickSort(int[] values) {
-        // check if array is empty or null
-        if (values == null || values.length == 0){
-            return;
-        }
-        this.numbers = values;
-        number = values.length;
-        quicksort(0, number - 1);
-    }
+            int point = point(arr, start, end);
 
-    private void quicksort(int low, int high) {
-        int i = low, j = high;
-        // Get middle element of the list
-        int middle = numbers[low + (high-low)/2];
-
-        // Divide into two lists
-        while (i <= j) {
-
-            // If the current value from the left list is smaller than the middle
-            // element then get the next element from the left list
-            while (numbers[i] < middle) {
-                i++;
+            if(point-1>start) {
+                quickSort(arr, start, point - 1);
             }
-            // If the current value from the right list is larger than the middle
-            // element then get the next element from the right list
-            while (numbers[j] > middle) {
-                j--;
-            }
-
-            // If we have found a value in the left list which is larger than
-            // the middle element and if we have found a value in the right list
-            // which is smaller than the middle element then we exchange the
-            // values.
-            // As we are done we can increase i and j
-            if (i <= j) {
-                changeNumbers(i, j);
-                i++;
-                j--;
+            if(point+1<end) {
+                quickSort(arr, point + 1, end);
             }
         }
-        // Recursion
-        if (low < j)
-            quicksort(low, j);
-        if (i < high)
-            quicksort(i, high);
-    }
 
-    private void changeNumbers(int i, int j) {
-        int temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
-    }
-}
+        public static int point(int[] arr, int start, int end){
+            int pivot = arr[end];
 
+            for(int i=start; i<end; i++){
+
+                //if element x is smaller than the end of the array
+                if(arr[i]<pivot){
+
+                    //change the elements; the first element gets a temporary place
+                    int temp= arr[start];
+                    // the smaller element gets the first place
+                    arr[start]=arr[i];
+                    //now the first element gets the index from the actually first element
+                    arr[i]=temp;
+                    //count iteration
+                    start++;
+                }
+            }
+
+            int temp = arr[start];
+            arr[start] = pivot;
+            arr[end] = temp;
+
+            return start;
+        }
+    }

716a399bb1dd851ab4b45de8050fdb089c470438
caro999999 <50207918+caro999999@users.noreply.github.com>
2019-06-10 09:34:17 +0200

    Update QuickSort.java

--- src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
+++ src/de/dhbw/ravensburg/SortingAlgorithms/QuickSort.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class QuickSort {
 
+
+    /**
+     *
+     * @param arr array which has to be sorted
+     * @param start at which index do we want to start the quicksort
+     * @param end to which index do we want to sort?
+     */
         public static void quickSort(int[] arr, int start, int end){
 
             int point = point(arr, start, end);
@@ -16,12 +23,22 @@ public class QuickSort {
             }
         }
 
+
+    /**
+     *
+     * @param arr array which has to be sorted
+     * @param start at which index do we want to start the quicksort
+     * @param end to which index do we want to sort?
+     * @return the new and final index from the pivot
+     */
         public static int point(int[] arr, int start, int end){
+
+            //pivot is the separating element
             int pivot = arr[end];
 
             for(int i=start; i<end; i++){
 
-                //if element x is smaller than the end of the array
+                //if the element at this index is smaller than the pivot element of the array
                 if(arr[i]<pivot){
 
                     //change the elements; the first element gets a temporary place
@@ -34,6 +51,8 @@ public class QuickSort {
                     start++;
                 }
             }
+
+            //the pivot gets its right position, the smaller elements are left, the bigger right
 
             int temp = arr[start];
             arr[start] = pivot;
